a2bc410b3be02b6423aefb0a90aa5faa5caf5656
levalyukov <lev.alyukov@gmail.com>
2025-01-08 21:47:09 +0500

    Update settings.js

--- assets/core/settings.js
+++ assets/core/settings.js
@@ -1,3 +1,16 @@
 function AppSettings() {
-    // 
+    if (localStorage.getItem('settings')) {
+        const key = localStorage.key('settings')
+        const appData = JSON.parse(localStorage.getItem(key))
+        if ('theme' in appData) {
+            document.documentElement.setAttribute('theme', appData.theme)
+        }
+    } else {
+        const appData = {
+            'theme': 'light',
+            'language': 'ru',
+        }
+        localStorage.setItem('settings', JSON.stringify(appData))
+        AppSettings()
+    }
 }
